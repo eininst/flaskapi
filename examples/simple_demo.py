3235c72ec8b7e3cac4f4aed0976e893fe2a6f3ce
王梓清 <9464553+superwzq1993@user.noreply.gitee.com>
2024-10-28 15:11:31 +0800

    init

---
+++ examples/simple_demo.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from pydantic import BaseModel, Field
+from flaskapi import Info, OpenAPI, Tag
+
+
+class Unauthorized(BaseModel):
+    code: int = Field(-1, description="Status Code")
+    message: str = Field("Unauthorized!", description="Exception Information")
+
+info = Info(title="book API", version="1.0.0")
+app = OpenAPI(__name__,
+              info=info, responses={"401": Unauthorized})
+
+book_tag = Tag(name="book", description="Some Book")
+
+
+class BookQuery(BaseModel):
+    age: int
+    author: str
+
+
+@app.get("/openapi", summary="get books", tags=[book_tag])
+def get_book(query: BookQuery) -> BookQuery:
+    """
+    get all books
+    """
+
+    return {
+        "code": 0,
+        "message": "ok",
+        "data": [
+            {"bid": 1, "age": query.age, "author": query.author},
+            {"bid": 2, "age": query.age, "author": query.author}
+        ]
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
